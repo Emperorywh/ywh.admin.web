a4e979224921c44029c3d81ae33d5d2121d2ed32
ywh <1289978696@qq.com>
2023-05-19 18:25:49 +0800

    优化日志

--- src/pages/logs/requestLog/components/RequestLogModal.tsx
+++ src/pages/logs/requestLog/components/RequestLogModal.tsx
@@ -1,6 +1,6 @@
 import { IRequestLog } from "@/api/logs";
-import { Modal, Typography } from "antd";
-import { JSONTree } from 'react-json-tree';
+import { Descriptions, Modal } from "antd";
+import { ProField, ProFieldFCMode } from "@ant-design/pro-components";
 
 export interface IProps {
     requestLog: IRequestLog;
@@ -14,21 +14,67 @@ export interface IProps {
  * @returns 
  */
 const RequestLogModal: React.FC<IProps> = (props) => {
-    console.log('requestLog', props.requestLog);
     const { requestLog } = props;
-    return <Modal width={1200} title="请求日志" open={props.open} onOk={props.onOk} onCancel={props.onCancel}>
-        <Typography>
-            <Typography.Title >{requestLog.responseStatus}</Typography.Title>
-            <Typography.Paragraph copyable>请求地址：{requestLog.url}</Typography.Paragraph>
-            <Typography.Paragraph >请求方法：{requestLog.method}</Typography.Paragraph>
-            <Typography.Paragraph copyable>请求参数：{JSON.stringify(requestLog.requestBody)}</Typography.Paragraph>
-            <JSONTree hideRoot data={requestLog.requestBody} />
-            <Typography.Paragraph>请求IP：{requestLog.ip}</Typography.Paragraph>
-            <Typography.Paragraph copyable>响应结果：{JSON.stringify(requestLog.responseBody)}</Typography.Paragraph>
-            <JSONTree hideRoot data={requestLog.responseBody} />
-            <Typography.Paragraph>响应消息：{requestLog.message}</Typography.Paragraph>
-            <Typography.Paragraph>请求时间：{new Date(requestLog.timestamp).toLocaleString()}</Typography.Paragraph>
-        </Typography>
+    return <Modal width={800} title="请求日志" open={props.open} onOk={props.onOk} onCancel={props.onCancel}>
+        <Descriptions column={1}>
+            <Descriptions.Item label="状态码">
+                <ProField
+                    text={requestLog.responseStatus}
+                    valueType="text"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+            <Descriptions.Item label="请求地址">
+                <ProField
+                    text={requestLog.url}
+                    valueType="text"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+            <Descriptions.Item label="请求方法">
+                <ProField
+                    text={requestLog.method}
+                    valueType="text"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+            <Descriptions.Item label="请求参数">
+                <ProField
+                    text={JSON.stringify(requestLog.requestBody)}
+                    valueType="jsonCode"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+            <Descriptions.Item label="IP地址">
+                <ProField
+                    text={requestLog.ip}
+                    valueType="text"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+            <Descriptions.Item label="响应结果" style={{overflow: 'auto'}}>
+                <ProField
+                    text={JSON.stringify(requestLog.responseBody)}
+                    valueType="jsonCode"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+            <Descriptions.Item label="响应消息">
+                <ProField
+                    text={requestLog.message}
+                    valueType="text"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+            <Descriptions.Item label="请求时间">
+                <ProField
+                    text={requestLog.timestamp}
+                    valueType="dateTime"
+                    mode={'read'}
+                />
+            </Descriptions.Item>
+        </Descriptions>
+
     </Modal>
 }
 
